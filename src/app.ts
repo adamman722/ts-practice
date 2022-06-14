39826d13229853cc8daa11bef2b9354d253b7238
adamman722 <tarpeyadam@gmail.com>
2022-06-14 15:56:49 -0400

    Added a test

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 class Department {
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
+  //switching it to protected it allows all the calssed derivied from this class to use it but anything outside it cannot be used
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -21,11 +22,13 @@ class Department {
   printEmployeeInformation() {
     console.log(this.employees.length);
     console.log(this.employees);
+    console.log(this.employees + "Nugget");
   }
 }
 
 class ITDepartment extends Department {
   admins: string[];
+  //to asigin any new props you must use the "this" keyword and must be after super
   constructor(id: string, admins: string[]) {
     super(id, "IT");
     this.admins = admins;
@@ -33,12 +36,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    return this.lastReport;
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+  }
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
   }
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -63,9 +79,14 @@ const accounting = new AccountingDepartment("d2", []);
 
 accounting.addReport("Something went wrong...");
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
 accounting.printReports();
 
-console.log("testtt");
+accounting.printEmployeeInformation();
+
+console.log("thingsss");
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
 // accountingCopy.describe();
